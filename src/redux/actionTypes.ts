62cdad636bee2f249da14eb2ad058950151708e6
nishina555 <n.toshi.1.014@gmail.com>
2021-08-29 21:05:34 +0900

    Epicのトリガーとなるアクションを追加

--- src/redux/actionTypes.ts
+++ src/redux/actionTypes.ts
@@ -1,3 +1,4 @@
 export enum ActionTypes {
   SET_TODOS = "SET_TODOS",
+  FETCH_TODOS = "FETCH_TODOS",
 }
--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -6,9 +6,17 @@ type SetTodosAction = {
   payload: { todos: TodoItem[] };
 };
 
+type FetchTodoAction = {
+  type: ActionTypes.FETCH_TODOS;
+};
+
+export const fetchTodos = (): FetchTodoAction => ({
+  type: ActionTypes.FETCH_TODOS,
+});
+
 export const setTodos = (todos: TodoItem[]): SetTodosAction => ({
   type: ActionTypes.SET_TODOS,
   payload: { todos },
 });
 
-export type TodoActions = SetTodosAction;
+export type TodoActions = SetTodosAction | FetchTodoAction;
