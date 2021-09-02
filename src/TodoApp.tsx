99c186d7f1ce89329b34772ba163f3f5335bd3f5
nishina555 <n.toshi.1.014@gmail.com>
2021-09-02 20:46:31 +0900

    AddTodoとVisibilityFiltersを追加してモックを完成させる

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,12 +1,15 @@
-import React from "react";
 import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
+import VisibilityFilters from "./components/VisibilityFilters";
 import "./styles.css";
 
 export default function TodoApp() {
   return (
     <div className="todo-app">
       <h1>Todo List</h1>
+      <AddTodo />
       <TodoList />
+      <VisibilityFilters />
     </div>
   );
 }
